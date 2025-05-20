9f166064c52779ddbde16035a0259404b707e9f5
rtx1 <155051843@qq.com>
2025-05-20 18:36:06 +0800

    tmp

--- docker_cmd_rtx1.sh
+++ docker_cmd_rtx1.sh
@@ -2,8 +2,8 @@ img="nvcr.io/nvidia/pytorch:21.02-py3"
 # img="padim:0.1"
 
 docker run --rm  --gpus all --privileged=true  --workdir /git --name "fixmatch"  -e DISPLAY --ipc=host -d --rm  -p 5233:8889  \
--v /mnt/she_zhen_code/FixMatch-pytorch:/git/fixmatch/ \
--v /mnt/data/shezhen_original_data:/git/datasets/shezhen_original_data \
+-v /mnt/newdisk/she_zhen_code/FixMatch-pytorch:/git/fixmatch/ \
+-v /mnt/newdisk/shezhen_original_data:/git/datasets/shezhen_original_data \
 $img sleep infinity
 
 
--- run_fixmatch.sh
+++ run_fixmatch.sh
@@ -1,7 +1,7 @@
 #python -m pdb train.py --dataset cifar10 --num-labeled 4000 --arch wideresnet --batch-size 64 --lr 0.03 --expand-labels --seed 5 --out results/cifar10@4000.5  
 
 
-python   dataset/shezhen_data_processing.py
+#python   dataset/shezhen_data_processing.py
 python -m pdb  train.py --dataset shezhen --data_dir /git/datasets/fixmatch_dataset --num-labeled 4000 --arch wideresnet \
 --image_size 512 --batch-size 1 --eval-step 2  --lr 0.03 --expand-labels --seed 5 --out results/shezhen@4000.5
 
